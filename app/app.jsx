123f7185145f1247b6a9e551330e02dfda852509
binarygiant <lancelsims@gmail.com>
2016-07-29 06:15:45 -0500

    updates that were required for awesome (added foundation, jquery, etc.)

--- app/app.jsx
+++ app/app.jsx
@@ -6,13 +6,17 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
 ReactDOM.render(
-    <Router history={hashHistory}>
-        <Route path="/" component={Main}>
-            <Route path="about" component={About}></Route>
-            <Route path="examples" component={Examples}></Route>
-            <IndexRoute component={Weather}></IndexRoute>
-        </Route>
-    </Router>,
-    document.getElementById('app')
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+      <Route path="about" component={About}/>
+      <Route path="examples" component={Examples}/>
+      <IndexRoute component={Weather}/>
+    </Route>
+  </Router>,
+  document.getElementById('app')
 );
