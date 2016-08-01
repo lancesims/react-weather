60172282f333683ba5efe72d9c34b066708ea3b9
binarygiant <lancelsims@gmail.com>
2016-08-01 06:16:45 -0500

    the rad stuff

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,17 @@ var React = require('react');
 var About = (props) => {
     return (
       <div>
-        <h3>About</h3>
-        <p>Welcome to the about page</p>
+        <h1 className="text-center">About</h1>
+
+        <p>Do you like this app?</p>
+        <div className="switch large">
+          <input className="switch-input" id="yes-no" type="checkbox" name="exampleSwitch"/>
+          <label className="switch-paddle" for="yes-no">
+            <span className="show-for-sr">Do you like me?</span>
+            <span className="switch-active" aria-hidden="true">Yes</span>
+            <span className="switch-inactive" aria-hidden="true">No</span>
+          </label>
+        </div>
       </div>
     )
 }
