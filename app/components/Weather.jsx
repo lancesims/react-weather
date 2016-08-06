a9d9895c5cda9b1df59889d77d5d1f1d87864d34
binarygiant <lancelsims@gmail.com>
2016-08-06 18:45:07 -0500

    add nav search support

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -14,7 +14,9 @@ var Weather = React.createClass({
         var that = this;
         this.setState({
             isLoading: true,
-            errorMessage: undefined
+            errorMessage: undefined,
+            location: undefined,
+            temp: undefined
         });
 
 
@@ -32,6 +34,20 @@ var Weather = React.createClass({
                 });
                 console.log(e.message);
             });
+    },
+    componentDidMount: function() {
+      var location = this.props.location.query.location;
+      if(location && location.length > 0) {
+        this.handleSearch(location);
+        window.location.hash = '#/';
+      }
+    },
+    componentWillReceiveProps: function(newProps) {
+      var location = newProps.location.query.location;
+      if(location && location.length > 0) {
+        this.handleSearch(location);
+        window.location.hash = '#/';
+      }
     },
     render: function() {
         var {isLoading, temp, location, errorMessage} = this.state;
