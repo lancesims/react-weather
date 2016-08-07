27459f7e6f78dbdb41f4ed58e94df1c57d05aa18
binarygiant <lancelsims@gmail.com>
2016-08-06 19:26:16 -0500

    fixed the error modal shit with foundation

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var {Link} = require('react-router');
 
-var Examples = (props) => {
+var Examples = (props) => {//
     return (
       <div>
         <h1 className="text-center page-title">Examples</h1>
